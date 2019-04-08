e164caefffd281ccfd92e4ac01fec56277384547
Ozman K <mackiekbk@yahoo.com>
2019-04-07 21:02:37 -0400

    added an event listerner to 'design select'

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const $title = $('#title');
 const $otherTitle = $('#other-title');
 const $design = $('#design');
 const $color = $('#color');
-
 $name.focus();
 $otherTitle.hide();
 
@@ -19,10 +18,21 @@ $title.on('change', function(){
   }
 });
 
+
 $design.on('change', function (){
   if( $('#design option:selected').val() === 'js puns'){
-    $('#color > option:selected').each(function(index){
+    $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
+    $('#color option').show();
+    $('#color option').filter('[value="tomato"], [value="steelblue"], [value="dimgrey"]').hide();
 
-    })
+  } else if($('#design option:selected').val() === 'heart js'){
+    $('#color option[value="tomato"]').attr('selected', 'selected');
+    $('#color option').show();
+    $('#color option').filter('[value="darkslategrey"], [value="gold"], [value="cornflowerblue"]').hide();
+
+  } else {
+    $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
+    $('#color option').show();
   }
+
 });
