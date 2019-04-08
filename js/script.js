9c2f545ea65aaf0013ddc4d067fd170cbbf12649
Ozman K <mackiekbk@yahoo.com>
2019-04-08 15:06:12 -0400

    added an eventListener for the 'activities input checkbox

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const $title = $('#title');
 const $otherTitle = $('#other-title');
 const $design = $('#design');
 const $color = $('#color');
+const $activities = $('.activities');
 $name.focus();
 $otherTitle.hide();
 
@@ -21,18 +22,30 @@ $title.on('change', function(){
 
 $design.on('change', function (){
   if( $('#design option:selected').val() === 'js puns'){
-    $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
+    $('#color option').removeAttr("selected");
     $('#color option').show();
     $('#color option').filter('[value="tomato"], [value="steelblue"], [value="dimgrey"]').hide();
+    $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
+
 
   } else if($('#design option:selected').val() === 'heart js'){
-    $('#color option[value="tomato"]').attr('selected', 'selected');
+    $('#color option').removeAttr("selected");
     $('#color option').show();
     $('#color option').filter('[value="darkslategrey"], [value="gold"], [value="cornflowerblue"]').hide();
+    $('#color option[value="tomato"]').attr('selected', 'selected');
+
 
   } else {
+    $('#color option').removeAttr("selected");
     $('#color option[value="cornflowerblue"]').attr('selected', 'selected');
     $('#color option').show();
   }
 
+});
+$('.activities').on('change', function (){
+  if( $('.activities input[name="all"]').prop(":checked", true)){
+    $('.activities input[name="js-libs"]').attr('disabled', true);
+  } else {
+    $('.activities input').attr('disabled', false);
+  }
 });
